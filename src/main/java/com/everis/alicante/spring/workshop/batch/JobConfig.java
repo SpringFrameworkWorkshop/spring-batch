ee082ce6a3beadfd052c699d4ee1306a7b49c171
Antonio <antoniovizuete@gmail.com>
2018-09-05 12:26:15 +0200

    Ejemplo Reader, Processor & Writer

--- src/main/java/com/everis/alicante/spring/workshop/batch/JobConfig.java
+++ src/main/java/com/everis/alicante/spring/workshop/batch/JobConfig.java
@@ -1,27 +1,37 @@
 package com.everis.alicante.spring.workshop.batch;
 
 import com.everis.alicante.spring.workshop.batch.model.Transferencia;
+import com.everis.alicante.spring.workshop.batch.model.TransferenciaXML;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.DefaultFieldSetFactory;
+import org.springframework.batch.item.file.transform.FieldSetFactory;
 import org.springframework.batch.item.xml.StaxEventItemWriter;
 import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+import java.beans.PropertyEditor;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class JobConfig {
-
-  private static final Log logger = LogFactory.getLog(JobConfig.class);
 
   @Bean
   public FlatFileItemReader<Transferencia> reader(@Value("input/data.csv")Resource input) {
@@ -35,10 +45,22 @@ public class JobConfig {
   }
 
   @Bean
-  public StaxEventItemWriter<Transferencia> writer(@Value("file:output/data.xml") Resource output) {
+  public ItemProcessor<Transferencia, TransferenciaXML> processor(DateFormat sdfEspanol, Double cambioEuroDollar) {
+    return transferencia -> {
+      final TransferenciaXML xml = new TransferenciaXML();
+      xml.setBeneficiario(transferencia.getBeneficiario());
+      xml.setConcepto(transferencia.getConcepto());
+      xml.setFechaEspanola(sdfEspanol.format(transferencia.getFecha()));
+      xml.setImporteDolares(transferencia.getImporte() * cambioEuroDollar);
+      return xml;
+    };
+  }
+
+  @Bean
+  public StaxEventItemWriter<TransferenciaXML> writer(@Value("file:output/data.xml") Resource output) {
     final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-    marshaller.setClassesToBeBound(Transferencia.class);
-    return new StaxEventItemWriterBuilder<Transferencia>()
+    marshaller.setClassesToBeBound(TransferenciaXML.class);
+    return new StaxEventItemWriterBuilder<TransferenciaXML>()
       .name("writer")
       .rootTagName("transferencias")
       .resource(output)
@@ -47,14 +69,16 @@ public class JobConfig {
   }
 
   @Bean
-  public Step csvToXmlStep(StepBuilderFactory stepBuilderFactory,
-    FlatFileItemReader<Transferencia> reader, StaxEventItemWriter<Transferencia> writer) {
+  public Step csvToXmlStep(StepBuilderFactory stepBuilderFactory, FlatFileItemReader<Transferencia> reader,
+    ItemProcessor<Transferencia,TransferenciaXML> processor, StaxEventItemWriter<TransferenciaXML> writer) {
     return stepBuilderFactory.get("csvToXmlStep")
-      .<Transferencia, Transferencia>chunk(10)
+      .<Transferencia, TransferenciaXML>chunk(10)
       .reader(reader)
+      .processor(processor)
       .writer(writer)
       .build();
   }
+
   @Bean
   public Job csvToXmlJob(JobBuilderFactory jobBuilderFactory, Step csvToXmlStep) {
     return jobBuilderFactory.get("csvToXmlJob")
@@ -62,12 +86,4 @@ public class JobConfig {
       .build();
   }
 
-  @Bean
-  public Job job(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-    return jobBuilderFactory.get("job").start(
-      stepBuilderFactory.get("step1").tasklet(((stepContribution, chunkContext) -> {
-        logger.info("El job ha sido ejecutado");
-        return RepeatStatus.FINISHED;
-      })).build()).build();
-  }
 }
--- src/main/java/com/everis/alicante/spring/workshop/batch/model/TransferenciaXML.java
+++ src/main/java/com/everis/alicante/spring/workshop/batch/model/TransferenciaXML.java
@@ -1,16 +1,15 @@
 package com.everis.alicante.spring.workshop.batch.model;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
 @Data
 @XmlRootElement(name = "transferencia")
-public class Transferencia {
+public class TransferenciaXML {
   private String beneficiario;
-  private Date fecha;
-  private Double importe;
+  private String fechaEspanola;
+  private Double importeDolares;
   private String concepto;
 }
